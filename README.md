51340e397fc124ae0af14527ba98f34573f45e4f
moritanian <pickit2express@gmail.com>
2022-08-21 05:37:04 +0900

    Define each object interface

---
+++ README.md
@@ -0,0 +1,7 @@
+# GatherTown Automate
+
+
+## Reference
+https://www.notion.so/Gather-HTTP-API-3bbf6c59325f40aca7ef5ce14c677444
+https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063#d15f608469674bf0ac6b85808df01a8c
+https://gathertown.notion.site/WIP-Gather-object-data-format-c24e9c491fbd40db83649591339614a1
--- src/object/map.ts
+++ src/object/map.ts
@@ -12,12 +12,23 @@ export interface TownMap {
   spawns: Spawn[],
   spaces: Space[],
   objects: TownObject[],
-  collisions: string, // base64 encoded string from binary data. each byte is 0 (not impassable) or 1 (impassable) 
+  collisions: string, // base64 encoded string from binary data. each byte is 0 (not impassable) or 1 (impassable)
+  nooks: { [areaId: string]: Nook },
 }
 
+export interface Nook {
+  name: string;
+  nookCoords: {
+    coords: {
+      x: number,
+      y: number,
+    }
+  }
+};
 export interface Spawn {
   x: number,
   y: number,
+  spawnId?: string,
 };
 
 export interface Space {
@@ -34,17 +45,79 @@ export interface Portal {
   y: number,
 }
 
-export interface TownObject {
-  width: number,
-  height: number,
-  distThreshold?: number,
-  x: number,
-  y: number,
-  templateId: string,
-  id: string,
-  highlighted: string,
-  normal: string;
-  type: number;
-  _name: string;
-  orientation: (typeof ORIENTATION)[keyof typeof ORIENTATION];
+interface TownObjectCommon {
+  width: number; // the width of the image (pixels wide / 32)
+  height: number; // the height of the image (pixels tall / 32)
+  distThreshold?: number;
+  x: number; //  the x coordinate of the top left corner
+  y: number; // the y coordinate of the top left corner
+  id: string;
+  normal: string; // a link to an img src for how the object should show up when you're not within interaction range
+  highlighted: string; //a link to an img src for how the object should show up when you are within range (and this is the closest object)
+  orientation?: (typeof ORIENTATION)[keyof typeof ORIENTATION];
 }
+
+export interface NonInteractiveObject extends TownObjectCommon {
+  type: 0;
+}
+
+export interface IFrameObject extends TownObjectCommon {
+  type: 1;
+  properties: {
+    url: string;
+  }
+}
+
+
+export interface ImagePosterObject extends TownObjectCommon {
+  type: 2;
+  properties: {
+    image: string;  // the image you fullscreen when you press x
+    preview: string; // the preview image that pops up at the bottom when you're in range
+    // optional bonus fields
+    blurb: string; // text you can add that shows up when people hover on the fullscreened poster
+    loading: string; // the image/icon to show when the main image is loading after you interact
+  }
+}
+
+export interface VideoObject extends TownObjectCommon {
+  type: 3;
+  properties: {
+    video: string; // url of video you want. must be embedable
+    // optional property for synced TV's
+    startTime?: {
+      _timezone: string; // string, timezone, like "America/Los_Angeles"
+      _seconds: number; // number, Unix time
+      _nanoseconds: number;// number, always 0
+    }
+  }
+}
+
+export interface ExternalCallObject extends TownObjectCommon {
+  properties: {
+    zoomLink: string; // any url, usually to a zoom/webex/Meets/etc call
+  }
+}
+
+export interface NoteObject extends TownObjectCommon {
+  type: 6;
+  properties: {
+    message: string;// string, what shows up in the object
+  }
+}
+
+
+export interface ModalObject extends TownObjectCommon {
+  type: 7;
+  properties: {
+    extensionData: {
+      entries: Array<{
+        type: string;
+        value: string;
+        key: string;
+      }>
+    }
+  }
+}
+
+export type TownObject = NonInteractiveObject | IFrameObject | ImagePosterObject | VideoObject | ExternalCallObject | NoteObject | ModalObject;
